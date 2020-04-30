50ecfbf69ed9157da7f3ba671952238f5a095c64
deltagare <huiyi.skarner@gmail.com>
2020-04-30 09:21:23 +0200

    initial commit

---
+++ src/main/java/se/lexicon/java30/App.java
@@ -0,0 +1,13 @@
+package se.lexicon.java30;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
