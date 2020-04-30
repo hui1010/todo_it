cc5f3a0898d6b6ab2c61ea75fb30e78777cf932a
deltagare <huiyi.skarner@gmail.com>
2020-04-30 09:50:11 +0200

    made the person class

--- src/main/java/se/lexicon/java30/App.java
+++ src/main/java/se/lexicon/java30/App.java
@@ -1,9 +1,5 @@
 package se.lexicon.java30;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
---
+++ src/main/java/se/lexicon/java30/model/Person.java
@@ -0,0 +1,37 @@
+package se.lexicon.java30.model;
+
+public class Person {
+    private final int personId;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+        personId = getPersonId();
+    }
+
+    public Person(final int personId, String firstName, String lastName) {
+        this.personId = personId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+}
